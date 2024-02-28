58c2e2286b7fac1dd8e7abf7343693fe759853a6
Astrid Risa Widiana <astrdrisawww2212@gmail.com>
2024-02-28 12:04:25 +0700

    MODIFIKASI PRAKTIKUM 4 DB Facade

--- database/migrations/2024_02_28_042710_create_t_stok_table.php
+++ database/migrations/2024_02_28_042710_create_t_stok_table.php
@@ -14,11 +14,13 @@ return new class extends Migration
         Schema::create('t_stok', function (Blueprint $table) {
             $table->id('stok_id');
             $table->unsignedBigInteger('barang_id');
+            $table->unsignedBigInteger('user_id');
             $table->integer('stok_jumlah');
             $table->dateTime('stok_tanggal'); // Added stok_tanggal column
             $table->timestamps();
 
             $table->foreign('barang_id')->references('barang_id')->on('m_barang');
+            $table->foreign('user_id')->references('user_id')->on('m_user');
         });
     }
 
@@ -30,5 +32,5 @@ return new class extends Migration
     public function down()
     {
         Schema::dropIfExists('t_stok');
-    }
+}
 };
