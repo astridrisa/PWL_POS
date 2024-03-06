8abaafa61c99e6c5a2c040235a9d19b5d5e2888e
Astrid Risa Widiana <astrdrisawww2212@gmail.com>
2024-03-06 10:13:31 +0700

    Praktikum 2.4 Retreiving or Creating Models

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,18 +7,18 @@
         <h1>Data User</h1>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
-                <th>Jumlah Pengguna</th>
-                {{-- <th>ID</th>
+                {{-- <th>Jumlah Pengguna</th> --}}
+                <th>ID</th>
                 <th>Usernama</th>
                 <th>Nama</th>
-                <th>ID Level Pengguna</th> --}}
+                <th>ID Level Pengguna</th>
             </tr>
             <tr>
-                <td>{{ count($data) }}</td>
-                {{-- <td>{{ $user->user_id }}</td>
-                <td>{{ $user->username }}</td>
-                <td>{{ $user->nama }}</td>
-                <td>{{ $user->level_id }}</td> --}}
+                {{-- <td>{{ count($data) }}</td> --}}
+                <td>{{ $data->user_id }}</td>
+                <td>{{ $data->username }}</td>
+                <td>{{ $data->nama }}</td>
+                <td>{{ $data->level_id }}</td>
             </tr>
         </table>
     </body>
