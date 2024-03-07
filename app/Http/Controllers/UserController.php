899a94a94df602fc1c862dcb1d6894b6110e69fb
Astrid Risa Widiana <astrdrisawww2212@gmail.com>
2024-03-07 10:30:11 +0700

    Praktikum 2.7 Relationships

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,10 +6,16 @@ use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
+
 class UserController extends Controller
 {
     public function index()
     {
+        $user = UserModel::with('level')->get();
+        return view('user', ['data' => $user]);
+    }
+        // $user = UserModel::with('level')->get();
+        // dd($user);
     // $user = UserModel::create([
     //     'username' => 'manager55',
     //     'nama' => 'Manager55',
@@ -52,13 +58,16 @@ class UserController extends Controller
     // $user->wasChanged('nama'); //false
     // dd($user->wasChanged(['nama', 'username'])); //true
 
-    $user = UserModel::all();
-    return view('user', ['data' => $user]);
-    }
+
+    // $user = UserModel::all();
+    // return view('user', ['data' => $user]);
+    // }
+
     public function tambah()
     {
         return view('user_tambah');
     }
+
     public function tambah_simpan(Request $request)
     {
         UserModel::create([
@@ -69,11 +78,13 @@ class UserController extends Controller
         ]);
         return redirect('/user');
     }
+
     public function ubah($id)
     {
         $user = UserModel::find($id);
         return view('user_ubah', ['data' => $user]);
     }
+
     public function ubah_simpan($id, Request $request)
     {
         $user = UserModel::find($id);
@@ -87,6 +98,7 @@ class UserController extends Controller
 
         return redirect('/user');
     }
+
     public function hapus($id)
     {
         $user = UserModel::find($id);
