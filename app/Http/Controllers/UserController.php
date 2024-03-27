51e0971c8dcbc2473e2dd79706c799c1248f2747
Astrid Risa Widiana <astrdrisawww2212@gmail.com>
2024-03-27 09:06:44 +0700

    PERUBAHAN ERROR USERDATATABLE

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,42 +5,46 @@ namespace App\Http\Controllers;
 use App\DataTables\UserDataTable;
 use App\Http\Requests\StorePostRequest;
 use App\Models\UserModel;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\View\View;
 use Illuminate\Support\Facades\Hash;
-
-
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+ 
 class UserController extends Controller
 {
-    public function index(UserDataTable $dataTable):View
+    public function index(UserDataTable $dataTable)
     {
+        
         return $dataTable->render('user.index');
     }
-    public function create(): View
+
+    public function create()
     {
-        return view('user.create'); 
-     }
- 
-     public function store(StorePostRequest $request): RedirectResponse
-     {
+        return view('user.create');
+    }
+
+    public function store(StorePostRequest $request)
+    {
         // The incoming request is valid...
 
         // Retrieve the validated input data...
-        $request->validate();
+        $validated = $request->validate();
 
         // Retreive a portion of the validated input data...
-        $request->safe()->only(['username', 'nama', 'password', 'level_id']);
-        $request->safe()->except(['username', 'nama', 'password', 'level_id']);
-        
+        $validated = $request->safe()->only(['username', 'nama', 'password', 'level_id']);
+        $validated = $request->safe()->except(['username', 'nama', 'password', 'level_id']);
+
         UserModel::create([
             'username' => $request->username,
             'nama' => $request->nama,
-            'password' => Hash::make($request->password),
+            'password' => Hash::make('$request->password'),
             'level_id' => $request->level_id,
         ]);
-         return redirect('/user');
-     }
+
+        // Store the post
+
+        return redirect('/user');
+    }
 
     public function edit($id)
     {
@@ -50,11 +54,11 @@ class UserController extends Controller
 
     public function edit_simpan($id, Request $request)
     {
-        $user = UserModel :: find($id);
+        $user = UserModel::find($id);
 
         $user->username = $request->username;
         $user->nama = $request->nama;
-        $user->password = Hash::make( '$request->password');
+        $user->password = Hash::make('$request->password');
         $user->level_id = $request->level_id;
 
         $user->save();
@@ -64,11 +68,12 @@ class UserController extends Controller
 
     public function delete($id)
     {
-        $user = UserModel :: find($id);
+        $user = UserModel::find($id);
         $user->delete();
 
         return redirect('/user');
     }
+
         // $user = UserModel::with('level')->get();
         // dd($user);
     // $user = UserModel::create([
