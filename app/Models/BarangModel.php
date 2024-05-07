95f241559a6cd517172be2244611811edc7a152b
Astrid Risa Widiana <astrdrisawww2212@gmail.com>
2024-05-07 21:42:17 +0700

    TUGAS JOBSHEET 11 TABEL m_barang

--- app/Models/BarangModel.php
+++ app/Models/BarangModel.php
@@ -4,16 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class BarangModel extends Model
 {
     protected $table = 'm_barang';
     protected $primaryKey = 'barang_id';
     
-    protected $fillable = ['kategori_id', 'barang_kode','barang_nama', 'harga_beli', 'harga_jual', 'created_at', 'updated_at'];
+    protected $fillable = ['kategori_id', 'barang_kode','barang_nama', 'harga_beli', 'harga_jual', 'created_at', 'updated_at', 'image'];
     
     public function kategori(): BelongsTo
     {
         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image),
+        );
     }
 }
